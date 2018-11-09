f8f3f6cc6b3e44521c7fe13feac8d8162eada4b9
edle <eduard.lepner@gmail.com>
2018-11-09 23:18:09 +0100

    Added messages

---
+++ Messaging.Contracts/Class1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Messaging.Contracts
+{
+    public class Class1
+    {
+    }
+
+    public interface ChangeCaseCommand
+    {
+        string Message { get; set; }
+    }
+
+    public class ChangeCaseCommandImpl : ChangeCaseCommand
+    {
+        public string Message { get; set; }
+    }
+
+    public interface CaesarCipher
+    {
+        string Message { get; set; }
+        int Offset { get; set; }
+    }
+
+    public class CaesarCipherImpl : CaesarCipher
+    {
+        public string Message { get; set; }
+        public int Offset { get; set; }
+    }
+}
